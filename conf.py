8bee3bb7c24a6bad1decf9cad70888078e0c3e16
Kristian Rother <kristian@spiced-academy.com>
2025-05-24 18:50:55 +0200

    theme aufgeräumt

--- conf.py
+++ conf.py
@@ -6,10 +6,12 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Teaching Programming'
-copyright = '2023, Kristian Rother'
+project = 'Python Grundlagenkurs'
+copyright = '2025, Kristian Rother'
 author = 'Kristian Rother'
 release = '1.0'
+html_title = f"{project}"
+
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -21,16 +23,23 @@ extensions = [
     'myst_parser',
     ]
 
-templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', 'learning_goals.md', 'README.md']
 
-language = 'ls'
+language = 'de'
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'furo'
-#html_theme_path = ['themes']
-#html_static_path = ['_static']
-#html_logo = "_static/banner_wide.svg"
-#html_favicon = "_static/favicon.ico"
+html_static_path = ['_static']
+html_logo = "_static/academis_logo.png"
+html_favicon = "_static/favicon.ico"
+
+html_css_files = [
+    "academis.css",
+]
+html_theme_options = {
+    "source_repository": "https://github.com/krother/python_grundlagenkurs",
+    "source_branch": "master",
+    "source_directory": "",
+}
